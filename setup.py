f0529a79ba783f2039573965c06bd67ac3ebcffd
Desmond Brown <browndesmond30@yahoo.com>
2021-01-28 20:47:38 +0000

    Kali: Updated setup.py with long description

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="stonegrading",
     packages=find_packages(include=["stonegrading"]),
     version="0.1.0",
+    long_description_content_type="text/markdown",
+    long_description="Will add it soon",
     description="Diamond Stone Grading Package. It ships stone fields for reports and actual stones",
     author="Gradia Limited",
     license="MIT",
