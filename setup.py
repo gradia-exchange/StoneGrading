d00cd73a54c79c189ba6a9150e31f629aa25ec73
Desmond Brown <browndesmond30@yahoo.com>
2021-02-02 10:29:00 +0000

    Kali: Changed package name from stone-grading to stonegrading

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="stonegrading",
-    packages=find_packages(include=["stonegrading"]),
+    packages=find_packages(include=["stone-grading"]),
     version="0.1.0",
     long_description_content_type="text/markdown",
     long_description="Will add it soon",
