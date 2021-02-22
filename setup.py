9c99f5511b15895f11cd9e922b7782bdc73f4337
Desmond Brown <browndesmond30@yahoo.com>
2021-02-22 08:05:53 +0000

    Refactored setup to use static version for the right reason and use dynamic when necessary

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-from setuptools import find_packages, setup
+import setuptools 
 
-setup(
-    name="stone-grading",
-    packages=find_packages(include=["stonegrading"]),
-    version="0.1.0",
-    long_description_content_type="text/markdown",
-    long_description="Will add it soon",
-    description="Diamond Stone Grading Package. It ships stone fields for reports and actual stones",
-    author="Gradia Limited",
-    license="MIT",
-    install_requires=[],
-    setup_requires=["pytest-runner"],
-    tests_requires=["pytest==6.2.2"],
-    test_suite="tests",
-)
+
+setuptools.setup()
+
